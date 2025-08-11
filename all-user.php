bdff1a03c94477fecfba696de7c90c1d2c1a0872
Md Mahmudul Hossain Himel <mdmahmudulhossainhimel@gmail.com>
2025-08-11 17:17:46 +0600

    some code change

--- all-user.php
+++ all-user.php
@@ -55,19 +55,19 @@ get_sidebar();
             while ($dataFetch = mysqli_fetch_array($dataQuery)) {
             ?>
               <tr>
-                <td><?= $dataFetch['user_id'] ?></td>
-                <td><?= $dataFetch['full_name'] ?></td>
-                <td><?= $dataFetch['user_phone'] ?></td>
-                <td><?= $dataFetch['user_email'] ?></td>
-                <td><?= $dataFetch['user_name'] ?></td>
+                <td><?php echo $dataFetch['user_id']; ?></td>
+                <td><?= $dataFetch['full_name']; ?></td>
+                <td><?= $dataFetch['user_phone']; ?></td>
+                <td><?= $dataFetch['user_email']; ?></td>
+                <td><?= $dataFetch['user_name']; ?></td>
                 <td>---</td>
                 <td>
-                  <?php if($dataFetch['user_photo']!==''){ ?>
-                    <img class="rounded-2" height="40" src="upload-images/<?= $dataFetch['user_photo'] ?>" alt=""/>
-                  <?php } else{ ?>
-                    <img class="rounded-2" height="40" src="images/avatar.jpg" alt=""/>
+                  <?php if ($dataFetch['user_photo'] !== '') { ?>
+                    <img height="40" src="upload-images/<?= $dataFetch['user_photo']; ?>" alt="" />
+                  <?php } else { ?>
+                    <img height="40" src="images/avatar.jpg" alt="" />
                   <?php } ?>
-                </td> 
+                </td>
                 <td>
                   <div class="btn-group btn_group_manage" role="group">
                     <button type="button" class="btn btn-sm btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Manage</button>
