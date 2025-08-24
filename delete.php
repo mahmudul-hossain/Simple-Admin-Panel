5b9784ef5f3c9a02f9fa5059a7957a937c8b49c7
Md Mahmudul Hossain Himel <mdmahmudulhossainhimel@gmail.com>
2025-08-24 10:23:30 +0600

    delete file's add new code

--- delete.php
+++ delete.php
@@ -3,9 +3,22 @@
 require_once "all_function/functions.php";
 
 $userID=$_GET['delete'];
-$dataSelect="DELETE FROM user_table WHERE user_id='$userID'";
+$dataSelect="SELECT user_photo FROM user_table WHERE user_id='$userID'";
+$dataQuery=mysqli_query($connectDatabase,$dataSelect);
+$dataFetch=mysqli_fetch_array($dataQuery);
 
-if(mysqli_query($connectDatabase,$dataSelect)){
+$userPhoto=$dataFetch['user_photo'];
+
+if ($userPhoto!==''){
+    $photoPath="upload-images/".$userPhoto;
+    if(file_exists($photoPath)){
+        unlink($photoPath);
+    }
+}
+
+$dataDelete="DELETE FROM user_table WHERE user_id='$userID'";
+
+if(mysqli_query($connectDatabase,$dataDelete)){
     header("Location:all-user.php");
 }
 
